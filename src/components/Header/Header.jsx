c38c14ec83566c99f948ca13581dcecdacee0059
HassanAbbas10 <hassanabbas05674@gmail.com>
2024-04-29 10:50:49 +0500

    Modal Added for cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,44 @@
 import { Link } from "react-router-dom";
 import Search from "./../Search/Search";
+import Modal from "../Modal/Modal";
 
 const Header = () => {
   return (
-    <nav className="top-0 p-3 py-4 z-10 bg-whiterounded-b-md border-b border-solid 
+    <nav
+      className="top-0 p-3 py-4 z-10 bg-whiterounded-b-md border-b border-solid 
     border-slate-300 border-1 sticky backdrop-filter 
-    backdrop-blur-lg bg-opacity-30">
-    <div className="relative">
-      <div className="flex justify-between items-center px-2 ">
-        <div className="flex">
-        <p className="text-orange-300 text-3xl ">
-          Dev <span className="text-red-500 italic">Shop</span>
-        </p>
-        </div>
-     
-        <Search />
-        <div className="flex items-center justify-center list-none px-2 gap-10 text-white ">
-          <li className="active:text-orange-400">
-            <Link to="/" className="text-orange-500 italic">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/products" className="text-orange-500 italic ">
-              Shop
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" className="text-orange-500 italic">
-              Contact
-            </Link>
-          </li>
+    backdrop-blur-lg bg-opacity-30"
+    >
+      <div className="relative">
+        <div className="flex justify-between items-center px-2 ">
+          <div className="flex">
+            <p className="text-orange-300 text-3xl ">
+              Dev <span className="text-red-500 italic">Shop</span>
+            </p>
+          </div>
+
+          <Search />
+          <div className="flex items-center justify-center list-none px-2 gap-10 text-white ">
+            <li className="active:text-orange-400">
+              <Link to="/" className="text-orange-500 italic">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/products" className="text-orange-500 italic ">
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="text-orange-500 italic">
+                Contact
+              </Link>
+            </li>
+
+            <Modal />
+          </div>
         </div>
       </div>
-    </div>
     </nav>
   );
 };
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+
 const Search = () => {
   const [inputVal, setInputVal] = useState("");
   // export const [state, setState] = useState("");
@@ -7,14 +8,13 @@ const Search = () => {
 
   const handlechange = (e) => {
     setInputVal(e.target.value);
-    
   };
   console.log(inputVal);
   // const submitHandler = () => {
   //   setState(inputVal);
   // };
 
-  const apiUrl= `https://dummyjson.com/products/search?q=${inputVal}&limit=4`
+  const apiUrl = `https://dummyjson.com/products/search?q=${inputVal}&limit=4`;
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(apiUrl);
@@ -22,17 +22,14 @@ const Search = () => {
       setApiData(data.products);
     };
 
-    if (inputVal) { 
+    if (inputVal) {
       fetchData();
     }
   }, [apiUrl, inputVal]);
 
   return (
-    
     <div className="flex container max-w-lg flex-col">
-    <div className="flex items-center justify-center">
-      
-        
+      <div className="flex items-center justify-center">
         <input
           id="search"
           type="text"
@@ -40,25 +37,26 @@ const Search = () => {
           value={inputVal}
           className=" px-8 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 rounded-full w-96"
         />
-        
-        <button
-          
-          className="bg-orange-500 hover:bg-blue-300 text-white font-bold py-1 px-1.5 border-orange-400 rounded-full relative right-10"
-        >
-          <SearchIcon/>
+
+        <button className="bg-orange-500 hover:bg-blue-300 text-white font-bold py-1 px-1.5 border-orange-400 rounded-full relative right-10">
+          <SearchIcon />
         </button>
-        </div>
-        {apiData.length > 0 & inputVal != '' ? (
-      <ul className="absolute top-[2.60rem] px-2  bg-slate-200 w-[20rem] ml-7 rounded-b-md border border-orange-300">
-        {apiData.filter(filters=>filters.title.toLowerCase().includes(inputVal)).map((dat) => (
-          <li className="border-b  border-slate-400 py-2" key={dat.id} >{dat.title}</li>
-        ))}
-      </ul>
-    ):
-    <div></div>}
-    
+      </div>
+      {(apiData.length > 0) & (inputVal != "") ? (
+        <ul className="absolute top-[2.60rem] px-2  bg-slate-200 w-[20rem] ml-7 rounded-b-md border border-orange-300">
+          {apiData
+            .filter((filters) => filters.title.toLowerCase().includes(inputVal))
+            .map((dat) => (
+              <li className="border-b  border-slate-400 py-2" key={dat.id}>
+                {dat.title}
+              </li>
+            ))}
+        </ul>
+      ) : (
+        <div></div>
+      )}
     </div>
-  //FIXME:  Add the functional of seach items shown in cards
+    //FIXME:  Add the functional of seach items shown in cards
   );
 };
 
