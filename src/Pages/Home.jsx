be7e4af9f7b23d88fa3493bfec6e76b93d761c6e
HassanAbbas10 <hassanabbas05674@gmail.com>
2024-05-06 19:12:49 +0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Pages/Home.jsx 	new file:   src/components/Carousel/CarouselImg.jsx 	modified:   src/components/ui/button.jsx 	new file:   src/components/ui/carousel.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,13 @@
+import CarouselImg from "@/components/Carousel/CarouselImg";
+
 const Home = () => {
-  return <div>HOme</div>;
+  return <div>
+
+<CarouselImg/>
+
+
+
+  </div>;
 };
 
 export default Home;
