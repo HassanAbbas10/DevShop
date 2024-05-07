f8a654cef433938ae2090dffcb24a5a15b4b0534
HassanAbbas10 <hassanabbas05674@gmail.com>
2024-05-07 21:26:05 +0500

    modified:   package-lock.json 	modified:   src/components/Carousel/CarouselImg.jsx 	modified:   src/components/ui/button.jsx 	new file:   src/components/ui/card.jsx

--- src/components/Carousel/CarouselImg.jsx
+++ src/components/Carousel/CarouselImg.jsx
@@ -1,37 +1,34 @@
-
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-  } from "@/components/ui/carousel"
-  import { Card, CardContent } from "@/components/ui/card"
-  // import messages from "@/messages.json"
-  // import Autoplay from "embla-carousel-autoplay"
-  
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+// import messages from "@/messages.json"
+// import Autoplay from "embla-carousel-autoplay"
+
 const CarouselImg = () => {
   return (
-    
-       <Carousel className="w-full max-w-xs">
-    <CarouselContent>
-      {Array.from({ length: 5 }).map((_, index) => (
-        <CarouselItem key={index}>
-          <div className="p-1">
-            <Card>
-              <CardContent className="flex aspect-square items-center justify-center p-6">
-                <span className="text-4xl font-semibold">{index + 1}</span>
-              </CardContent>
-            </Card>
-          </div>
-        </CarouselItem>
-      ))}
-    </CarouselContent>
-    <CarouselPrevious />
-    <CarouselNext />
-  </Carousel>
-  
-  )
-}
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
 
-export default CarouselImg
+export default CarouselImg;
