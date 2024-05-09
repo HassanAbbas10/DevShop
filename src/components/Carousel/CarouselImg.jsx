81d643395df6c5fba4b4c758ae81aa526cf39f3a
HassanAbbas10 <hassanabbas05674@gmail.com>
2024-05-09 13:51:55 +0500

    new file:   src/CartContext.js 	modified:   src/components/Carousel/CarouselImg.jsx

--- src/components/Carousel/CarouselImg.jsx
+++ src/components/Carousel/CarouselImg.jsx
@@ -8,22 +8,44 @@ import {
 import { Card, CardContent } from "@/components/ui/card";
 // import messages from "@/messages.json"
 // import Autoplay from "embla-carousel-autoplay"
+import { useState,useEffect} from "react";
 
 const CarouselImg = () => {
+  const [apiDataa, setApiData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products?q&limit=5");
+        const data = await response.json();
+        setApiData(data);
+        console.log(data.products);
+      } catch (error) {
+        console.error(error,"its not working 🎃");
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <Carousel className="w-full max-w-xs">
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
+        {apiDataa.length > 0 ? (
+          <div>
+             {apiDataa.map((data) => (
+          <CarouselItem key={data.products.id}>
             <div className="p-1">
               <Card>
                 <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
+                  <img src={data.products.images[1]} className="" />
                 </CardContent>
               </Card>
             </div>
           </CarouselItem>
         ))}
+          </div>
+          
+         ) : <>not anything</> }
+       
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
