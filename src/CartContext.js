3024b2b5372be43aeb44a30e25ffac8d1a305ed6
Hassan Abbas <95937687+HassanAbbas10@users.noreply.github.com>
2024-05-12 15:02:16 +0500

    modified:   Steps.md 	modified:   src/CartContext.js 	modified:   src/Pages/Home.jsx

--- src/CartContext.js
+++ src/CartContext.js
@@ -1,1 +1,62 @@
-// import { useContext } from "react";
+import { useState, useEffect, createContext } from "react";
+
+export const CartContext = createContext({
+  items: [],
+  getprodQuantity: () => {},
+  addItem: () => {},
+  remItem: () => {},
+  delCart: () => {},
+  totalCost: () => {},
+});
+
+export const CartProvider = ({ children }) => {
+  const [cartProducts, setCartProducts] = useState([]);
+
+  //We are getting products quant from this one
+  const getprodQuantity = (id) => {
+    const quantity = quantity.find((product) => product.id === id)?.quantity;
+
+    if (quantity === undefined) {
+      return "༼ つ ◕_◕ ༽つ";
+    }
+    return quantity;
+  };
+
+  const addItem = (id) => {
+    const quantity = getprodQuantity(id);
+    if (quantity === 0) {
+      setCartProducts(...cartProducts, {
+        id: id,
+        quantity: 1,
+      });
+    }
+    else{
+        setCartProducts(
+
+            cartProducts.map(
+                products => 
+                    id === products.id?{
+                        ...products,quantity:products.quantity + 1
+    }:products
+
+
+            )
+        )
+        }
+
+    }
+  };
+
+  const contextValue = {
+    items: cartProducts,
+    getprodQuantity,
+    addItem,
+    remItem,
+    delCart,
+    totalCost,
+  };
+
+  return (
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+  );
+};
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,11 @@
 import CarouselImg from "@/components/Carousel/CarouselImg";
 
 const Home = () => {
-  return <div>
-
-<CarouselImg/>
-
-
-
-  </div>;
+  return (
+    <div>
+      <CarouselImg />
+    </div>
+  );
 };
 
 export default Home;
